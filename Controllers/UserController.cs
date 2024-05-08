cd1bf7d9d98f71a739ae71b2eca839035cbd23fd
ODL_User 1326360 <odl_user_1326360@azurehol1882.onmicrosoft.com>
2024-05-08 17:04:40 +0000

    The most significant changes are the implementation of various methods in the UserController class in the UserController.cs file and the modification of the bindingRedirect for the WebGrease library in the Web.config file. The methods implemented in the UserController class are crucial for the functionality of a CRUD application, while the modification in the Web.config file ensures that any version of the WebGrease library can be redirected to the new version.
    1. CRUD application 2.csproj file:
       - Addition of a reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library. This library provides a CodeDOM compiler for the .NET platform.
       - Removal of the previously added reference to the Microsoft.CodeDom.Providers.DotNetCompilerPlatform library. This suggests that the library was added and then removed, possibly due to it not being needed or causing issues.
       - Removal of a comment at the end of the file. This comment was likely removed for clarity or because it was no longer relevant.
    
    2. UserController.cs file:
       - Implementation of various methods in the UserController class. These methods include Index, Details, Create, Edit, and Delete, which are standard methods in a CRUD (Create, Read, Update, Delete) application. The changes involve returning views and manipulating a list of User objects.
    
    3. Web.config file:
       - Modification of the bindingRedirect for the WebGrease library. The oldVersion attribute was changed from "1.0.0.0-1.6.5135.21930" to "0.0.0.0-1.6.5135.21930". This change could be to ensure that any version of the library can be redirected to the new version.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,7 +1,7 @@
 using CRUD_application_2.Models;
 using System.Linq;
 using System.Web.Mvc;
- 
+
 namespace CRUD_application_2.Controllers
 {
     public class UserController : Controller
@@ -11,34 +11,51 @@ namespace CRUD_application_2.Controllers
         public ActionResult Index()
         {
             // Implement the Index method here
+            return View(userlist);
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
             // Implement the details method here
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
- 
+
         // GET: User/Create
         public ActionResult Create()
         {
             //Implement the Create method here
+            return View();
         }
- 
+
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
             // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
+
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
+
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
- 
+
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
@@ -48,19 +65,39 @@ namespace CRUD_application_2.Controllers
             // If successful, it redirects to the Index action to display the updated list of users.
             // If no user is found with the provided ID, it returns a HttpNotFoundResult.
             // If an error occurs during the process, it returns the Edit view to display any validation errors.
+
+            if (ModelState.IsValid)
+            {
+                var existingUser = userlist.FirstOrDefault(x => x.Id == id);
+                if (existingUser == null)
+                {
+                    return HttpNotFound();
+                }
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
- 
+
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
             // Implement the Delete method here
+            return View(userlist.FirstOrDefault(x => x.Id == id));
         }
- 
+
         // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
             // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                userlist.Remove(user);
+            }
+            return RedirectToAction("Index");
         }
     }
 }
